8dcdba9e697c69ab9ad3f93982118c80eea298d4
meftaul <meftaulhaque@gmail.com>
2023-05-30 22:17:47 +0600

    Init Commit

---
+++ src/main/java/com/meftaul/emm/EmployeeMealManagerApplication.java
@@ -0,0 +1,13 @@
+package com.meftaul.emm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMealManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMealManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meftaul/emm/EmployeeMealManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meftaul.emm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMealManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
